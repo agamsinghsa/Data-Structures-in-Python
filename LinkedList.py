e8a1ab93b67d3e2bd5520fff7f9a8341c9c31fd4
Agam <99758084+agamsinghsa@users.noreply.github.com>
2022-03-07 13:08:20 -0500

    Create LinkedList.py

---
+++ LinkedList.py
@@ -0,0 +1,34 @@
+class LinkedList:
+    def __init__(self, head=None):
+        self.head = head
+    
+    def insert(self, value):
+        if self.head is None:
+            self.head = Node(value)
+        else:
+            current_node = self.head
+            while current_node.right:
+                current_node = current_node.right
+            current_node.right = Node(value)
+        return self
+        
+    def remove(self, value):
+        if self.head.value == value:
+            self.head = self.head.right
+        else:
+            parent = self.head
+            current_node = self.head.right
+            while current_node:
+                if current_node.value == value:
+                    parent.right = current_node.right
+                current_node = current_node.right
+                parent = parent.right
+        return self
+      
+    def to_string(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.value, "-> ", end="")
+            current_node = current_node.right
+        print("None")
+        return self
